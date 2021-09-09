162f714d2688fa7ef0fdff1fa309587b60563827
Bruno Nunes <brunocmo.nunes@gmail.com>
2021-09-09 13:42:21 -0300

    Add generated CRUD folders for property and user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PropertiesModule } from './properties/properties.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [PropertiesModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
