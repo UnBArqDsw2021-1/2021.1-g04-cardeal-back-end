5d8e8c39976fcced8c61cd251262395aae43ddc6
Douglas-Castro <farias.douglascastro@gmail.com>
2021-09-16 16:14:12 -0300

    Update phones module
    Co-authored-by: Pedro Haick <pedrohaickzz@gmail.com>

--- src/modules/phones/phones.module.ts
+++ src/modules/phones/phones.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Phone } from './phone.entity';
+
+import { PhonesController } from './phones.controller';
+import { PhonesService } from './phones.service';
+import { Phone } from './entities/phone.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Phone])],
+  controllers: [PhonesController],
+  providers: [PhonesService],
 })
 export class PhonesModule {}
