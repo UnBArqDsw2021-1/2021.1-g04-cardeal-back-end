b7c9d48bbf77c38cfb3e39253de864c497d0c25a
Bruno Nunes <brunocmo.nunes@gmail.com>
2021-09-16 23:32:22 -0300

    Modify property entities to typeorm types
    Co-authored-by: gustavoduartemoreira <gustavoduartemoreira@gmail.com>

--- src/properties/entities/property.entity.ts
+++ src/properties/entities/property.entity.ts
@@ -1,22 +1,70 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
 export class Property {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   name: string;
+
+  @Column()
   city: string;
+
+  @Column()
   state: string;
+
+  @Column()
   district: string;
+
+  @Column()
   street: string;
+
+  @Column()
   number: number;
+
+  @Column()
   zipNumber: string;
+
+  @Column()
   type: string;
+
+  @Column()
   size: string;
+
+  @Column()
   numberBedroom: number;
+
+  @Column()
   numberBath: number;
+
+  @Column()
   numberPark: number;
+
+  @Column()
   status: string;
+
+  @Column()
   value: number;
+
+  @Column()
   viewed: number;
+
+  @Column()
   idOwner: number;
+
+  @Column()
   idRealtor: number;
-  createdAt: number;
-  updatedAt: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
 }
