0130108be731dc22b97e8ac6bcdb964f705bb2d0
Bruno Nunes <brunocmo.nunes@gmail.com>
2021-09-20 20:43:49 -0300

    Change properties folder locationi to modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PropertiesModule } from './properties/properties.module';
+import { PropertiesModule } from './modules/properties/properties.module';
 import { ClientsModule } from './modules/clients/clients.module';
+import { PhonesModule } from './modules/phones/phones.module';
 
 @Module({
-  imports: [PropertiesModule, ClientsModule],
+  imports: [PropertiesModule, ClientsModule, PhonesModule],
   controllers: [AppController],
   providers: [AppService],
 })
