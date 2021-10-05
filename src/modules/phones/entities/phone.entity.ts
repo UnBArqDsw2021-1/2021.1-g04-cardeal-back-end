fa4013db46600b7e95562ed458d55f4d35f2fd97
Bruno Nunes <brunocmo.nunes@gmail.com>
2021-10-05 19:03:47 -0300

    Add owners to phone list

--- src/modules/phones/entities/phone.entity.ts
+++ src/modules/phones/entities/phone.entity.ts
@@ -1,6 +1,7 @@
 import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
 import { Client } from '../../clients/entities/client.entity';
 import { Realtor } from '../../realtor/entities/realtor.entity';
+import { Owner } from '../../owner/entities/owner.entity';
 
 @Entity()
 export class Phone {
@@ -21,4 +22,10 @@ export class Phone {
     onUpdate: 'CASCADE',
   })
   realtor: Realtor;
+
+  @ManyToOne(() => Owner, (owner) => owner.phones, {
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+  })
+  owner: Owner;
 }
