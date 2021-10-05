c09c8d57af21eaa7d602e5461f44c02e679990bf
Bruno Nunes <brunocmo.nunes@gmail.com>
2021-10-05 18:43:03 -0300

    Add realtors to phone list

--- src/modules/phones/dto/update-phone.dto.ts
+++ src/modules/phones/dto/update-phone.dto.ts
@@ -2,5 +2,8 @@ import { PartialType } from '@nestjs/mapped-types';
 import { CreatePhoneDto } from './create-phone.dto';
 
 export class UpdatePhoneDto extends PartialType(CreatePhoneDto) {
+  name: string;
+  cpf: string;
+  email: string;
   phone_number: string;
 }
