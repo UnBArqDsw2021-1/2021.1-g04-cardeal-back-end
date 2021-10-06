16e90f269f1e0cd30fd528e674262ce3533fa460
Bruno Nunes <brunocmo.nunes@gmail.com>
2021-10-06 13:01:10 -0300

    Update create|update dtos

--- src/modules/realtor/dto/update-realtor.dto.ts
+++ src/modules/realtor/dto/update-realtor.dto.ts
@@ -2,6 +2,9 @@ import { PartialType } from '@nestjs/mapped-types';
 import { CreateRealtorDto } from './create-realtor.dto';
 
 export class UpdateRealtorDto extends PartialType(CreateRealtorDto) {
+  name: string;
+  cpf: string;
+  email: string;
   passwordHash: string;
   phones: [];
 }
