bb6e7af4911be160bce988f47ebc454e4d73d336
Douglas-Castro <farias.douglascastro@gmail.com>
2021-10-08 12:03:17 -0300

    Add cors settings

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',
+    methods: '*',
+    allowedHeaders: '*',
+    credentials: true,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
